eaddaa3e097dbe31b2c81824e50216652c561cce
Joelle DesChatelets <jdeschat@gmail.com>
2021-07-09 15:48:42 -0400

    completed assignment

--- Develop/script.js
+++ Develop/script.js
@@ -10,17 +10,22 @@ var allCharacters = "";
 
 // create password
 var createPass = function () {
+  // reset characters, password & length
+  allCharacters = "";
+  password = "";
+  passLength = 0;
+  // ensure code is between 8-128 characters
   while (passLength < 8 || passLength > 128) {
     passLength = window.prompt(
       "Please type a number between 8 and 128 for your password length"
     );
   }
-
+  // windows asking which character type they would like included in their password
   var lowercaseConfirm = window.confirm("Would you like lowercase letters in your password? Click OK if yes");
   var uppercaseConfirm = window.confirm("Would you like uppercase letters in your password? Click OK if yes");
   var numbersConfirm = window.confirm("Would you like numbers in your password? Click OK if yes");
   var specialConfirm = window.confirm("Would you like special characters in your password? Click OK if yes");
-
+  // adding the character types they wish to use in their password
   if (lowercaseConfirm) {
     allCharacters += lowercase;
   };
@@ -37,14 +42,13 @@ var createPass = function () {
   console.log(allCharacters);
 }
 
-
+// generate password length
 function generatePassword() {
   createPass();
   for (let i = 1; i <= passLength; i++) {
-    const randomCharacter = allCharacters[Math.floor(Math.random() * allCharacters.length)]
-    password += randomCharacter;
+    let randomNumber = Math.floor(Math.random() * allCharacters.length)
+    password += allCharacters[randomNumber];
   }
-
   return password;
 }
 
