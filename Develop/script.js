1d07c1b1949607f037f877736d8960721b23f45f
Joelle DesChatelets <jdeschat@gmail.com>
2021-07-09 14:07:58 -0400

    made lowercase letters

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,59 @@
+// Characters code
+var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numeric = '123456789';
+var specialCharacters = '!@#$%^&*_-+=';
+var storePassword = "generated password";
+var passLength = 0;
+var password = "";
+var allCharacters = "";
+
+// create password
+var createPass = function () {
+  while (passLength < 8 || passLength > 128) {
+    passLength = window.prompt(
+      "Please type a number between 8 and 128 for your password length"
+    );
+  }
+
+  var lowercaseConfirm = window.confirm("Would you like lowercase letters in your password? Click OK if yes");
+  var uppercaseConfirm = window.confirm("Would you like uppercase letters in your password? Click OK if yes");
+  var numbersConfirm = window.confirm("Would you like numbers in your password? Click OK if yes");
+  var specialConfirm = window.confirm("Would you like special characters in your password? Click OK if yes");
+
+  if (lowercaseConfirm) {
+    allCharacters += lowercase;
+  };
+}
+
+
+function generatePassword() {
+  createPass();
+  for (let i = 1; i <= passLength; i++) {
+    const randomNumber = password[Math.floor(Math.random() * password.length)]
+    newPassword += randomNumber;
+  }
+
+  return password;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
